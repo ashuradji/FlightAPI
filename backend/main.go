73740b2247e703241a8e4a6e907d444fbcfdd272
ashuradji <45756472+ashuradji@users.noreply.github.com>
2025-04-20 18:01:33 -0600

    Hello World using Gin

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 func main() {
-	println("Hello World!")
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	r.Run(":80")
 }
